f3f5aaf024b3995a3b8195a3523c2bf2f3580b4c
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-09-18 23:14:57 +0530

    dkf

--- learn-terraform/4.data_sourse/datasourse.tf
+++ learn-terraform/4.data_sourse/datasourse.tf
@@ -1,6 +1,11 @@
 data "aws_ec2_spot_price" "ex"{
-    instance_type="t3.micro"
+    instance_type="t2.micro"
     availability_zone="us-east-1a"
+
+    filter{
+        name="product-description"
+        values=["Linux/UNIX"]
+    }
 }
 
 output "price"{
