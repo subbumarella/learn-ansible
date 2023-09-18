05534b6259e8a29fd88aa9c70bc629f985f3f8c6
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-09-18 15:12:38 +0530

    updated

---
+++ learn-terraform/variables/2.variablesAssignment.tf
@@ -0,0 +1,27 @@
+```hcl
+variable "instance_type"{
+    description="EC2 instance_type"
+    type=string
+    default="t2.micro"
+}
+variable "ami"{
+    description="ami"
+    default="ami-03265a0778a880afb"    
+}
+variable "tag_Name"{
+    default="sample instance"
+}
+
+resource "aws_instance" "ex"{
+    ami=var.ami
+    instance_type=var.instance_type
+    tags={
+        Name=var.tag_Name
+    }
+} 
+
+output "Ip_Address"
+{
+    description="Getting public ip"
+    value="The ip Address is - ${aws_instance.ex.ip_address}"
+}
