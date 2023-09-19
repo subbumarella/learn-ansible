e5f9738db840d462abcf024065d47437835f5e06
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-09-19 23:56:30 +0530

    updated

--- learn-terraform/7.module-1/main.tf
+++ learn-terraform/7.module-1/main.tf
@@ -8,10 +8,10 @@ module "sample2"{
     name="sample2"
 }
 
-output "public_ip_Sample1"{
+output "public_ip_sample1" {
     value=module.sample1.public_ip
 }
 
-output "public_ip_sample_2"{
+output "public_ip_sample2"{
     value=module.sample2.public_ip
 }
