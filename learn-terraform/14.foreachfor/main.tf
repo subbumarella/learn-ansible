562c46a28f489df983c3ba42174803f3115a5ca2
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-09-28 18:44:58 +0530

    updated

--- learn-terraform/14.foreachfor/main.tf
+++ learn-terraform/14.foreachfor/main.tf
@@ -10,11 +10,7 @@ variable "user_names"{
     default=["frontend","mongodb","catalogue"]
 }
 
-locals{
-    my_list=tolist(var.user_names)
-}
-
-resource "aws_iam_user" "ex"{
+resource "aws_instance" "ex"{
     for_each = { for idx , value in local.my_list: idx=>value}
     ami=data.aws_ami.ami_ex.id
     instance_type=t2.micro
