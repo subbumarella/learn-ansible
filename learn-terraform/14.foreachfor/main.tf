fd361e65c7c8a427f56895edd08cd458e750fa6a
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-09-28 18:47:34 +0530

    updae

--- learn-terraform/14.foreachfor/main.tf
+++ learn-terraform/14.foreachfor/main.tf
@@ -11,7 +11,7 @@ variable "user_names"{
 }
 
 resource "aws_instance" "ex"{
-    for_each = { for idx , value in local.my_list: idx=>value}
+    for_each = { for idx , value in var.user_names: idx=>value}
     ami=data.aws_ami.ami_ex.id
     instance_type=t2.micro
     name=each.value
