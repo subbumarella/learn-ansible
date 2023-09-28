0433c624d9deae72ffad55140c90cc167ae810dc
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-09-28 13:26:03 +0530

    codeupdated

--- learn-terraform/10.loopswithCount/main.tf
+++ learn-terraform/10.loopswithCount/main.tf
@@ -8,7 +8,7 @@ data "aws_ami" "ami_ex" {
 variable "user_name" {
     description="IAM usernames"
     type=list(string)
-    default=["user1,user2,user3"]
+    default=["user2","user3","user4"]
 }
 
 resource "aws_iam_user" "ex"    {
