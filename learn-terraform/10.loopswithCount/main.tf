4e9df3c15daff774b9106882c348551e485b6b15
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-09-28 12:59:54 +0530

    added loopswithCount

---
+++ learn-terraform/10.loopswithCount/main.tf
@@ -0,0 +1,18 @@
+
+data "aws_ami" "ami_ex{
+    owners=["973714476881"]
+    most_recent=true
+    name_regex="Centos-8-DevOps-Practice"
+}
+
+
+variable "user_name" {
+    description="IAM usernames"
+    type=list(string)
+    default=["user1,user2,user3"]
+}
+
+resource "aws_iam_user" "ex"{
+    count= length(var.user_name)
+    name=var.user_name[count.index]
+}
