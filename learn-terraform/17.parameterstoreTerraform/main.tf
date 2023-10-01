0698c4e21865ea089bcd4e10ca08447726a646cd
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-10-01 14:28:41 +0530

    UPDATED

--- learn-terraform/17.parameterstoreTerraform/main.tf
+++ learn-terraform/17.parameterstoreTerraform/main.tf
@@ -9,5 +9,9 @@ variable "params"{
     default=[
         {name="name1",type="String",value="value1"},
         {name="name2",type="String",value="value2"}
+        {name="name3",type="String",value="value3"},
+        {name="name4",type="String",value="value4"}
+        {name="name5",type="String",value="value5"},
+        {name="name6",type="String",value="value6"}
     ]
 }
